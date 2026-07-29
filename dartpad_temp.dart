21fa888fb273f652420152e7569d636f36b3d88b
durvesh <thes86088@gmail.com>
2026-07-29 23:48:40 +0530

    create function to change required cells
    the code will still not work since this app will require app-wide state management instead of simple class-wide state management

--- dartpad_temp.dart
+++ dartpad_temp.dart
@@ -12,9 +12,9 @@ class MainApp extends StatelessWidget {
 		return MaterialApp(
 		           home: Scaffold(
 		               body:  Center(
-                     child : Expanded (
-                       child: Canvas(8)                       
-                     )	                   
+                     //child : Expanded (
+                       child: Canvas(8)                      
+                     //)	                   
 		               )
 		            ),
 		       );
@@ -22,35 +22,42 @@ class MainApp extends StatelessWidget {
 }
 
 class CellData {
-  CellData(this.row, this.col, /*this.canvasSize*/);
+  CellData(this.row, this.col, this.stateString /*this.canvasSize*/);
  
   final int row ;
   final int col ;
+  String stateString ;
 }
 
 class Cell extends StatefulWidget{
-  Cell(this.cellData, /*this.canvasSize,*/{super.key});
+  Cell(this.cellData, this.triggerChanges, /*this.canvasSize,*/{super.key});
  
-  final CellData cellData;  
+  final CellData cellData;
+  final void Function( int, int ) triggerChanges ;
   @override
   State<Cell> createState() => _CellState();
 }
 
 class _CellState extends State<Cell> {
-  String state = 'DEFAULT'; //DEFAULT, WITHQUEEN, ATTACKALE
+  //String state = 'D' ;
+  //String state = widget.cellData.state_string; //DEFAULT, WITHQUEEN, ATTACKALE
   bool isPlaced = false ;
   
   Color decodeState( String state ){
     Color result = Colors.yellow ;
+    /*when same color is repeated, 
+    try adding a new color at either place 
+    to test/ensure proper logic
+    */
     
-    if( state == 'WITHQUEEN' ){
+    if( state == 'Q' ){
       result = Colors.green ;
     }
     else{
-      if( state == 'ATTACKABLE' ){
+      if( state == 'A' ){
         result = Colors.lime ;
       }
-      else {result = Colors.white ; }
+      //else {result = Colors.white ; }
     }
     return result;
   }
@@ -60,14 +67,16 @@ class _CellState extends State<Cell> {
     return Container( 
       //color : decodeState(state), 
       decoration: BoxDecoration(
-          color: decodeState(state),
+          color: decodeState( widget.cellData.stateString ),
           border: Border.all(color: Colors.grey.shade400, width: 0.5)
         ),
       child : GestureDetector(
         onTap : () { 
           setState( () { 
-            state = (state == 'WITHQUEEN') ? 'DEFAULT' : 'WITHQUEEN' ;
-           }); 
+            widget.cellData.stateString = 
+            ( widget.cellData.stateString == 'Q') ? 'D' : 'Q' ;
+           }
+         ); 
         } 
       )                  
    ); 
@@ -87,12 +96,12 @@ class Canvas extends StatefulWidget{
 }
 
 class _CanvasState extends State<Canvas>{
-  late List<List<CellData>> canvas ;
+  late List<List<CellData>> canvasData ;
   
   @override
   void initState() {
     super.initState();
-    canvas = _initCanvas();
+    canvasData = _initCanvas();
   }
    
   List<List<CellData>> _initCanvas( ){
@@ -100,11 +109,30 @@ class _CanvasState extends State<Canvas>{
     for( var row = 0; row < widget.canvasSize; row++  ){
       List<CellData> tempRow = [];
       for( var col = 0; col < widget.canvasSize; col++  ){
-        tempRow.add( CellData( row, col ) );
+        tempRow.add( CellData( row, col, 'D' ) );
       }
       result.add( tempRow );
     }
     return result ;
+  } 
+  
+  void highlightAttackableCells( int epicenterRow, int epicenterCol ){
+    List<List<CellData>> newCanvasData = [];
+    for( int row = 0; row < epicenterRow ; row++ ){
+      List<CellData> newTempRow = [];
+      for( int col = 0; col < epicenterCol; col++ ){
+        CellData tempCellData = CellData( row, col, 'D' );
+        if( row == epicenterRow || col == epicenterCol ){
+           if( canvasData[row][col].stateString == 'D' ) {
+             tempCellData.stateString = 'A' ;
+             //newCanvasData[row][col].stateString = 'A'; 
+           }
+        }
+        newTempRow.add( tempCellData );
+      }
+      newCanvasData.add( newTempRow );
+    }
+    setState( (){ canvasData = newCanvasData ; } );
   } 
 
     @override
@@ -119,7 +147,8 @@ class _CanvasState extends State<Canvas>{
               children : [ SizedBox( width : 10 ), Cell( canvas[row][col] )] 
             );
            */
-          tempRowChildren.add( Cell( canvas[row][col] ) ) ;
+          tempRowChildren.add( Cell( canvasData[row][col], 
+                                    highlightAttackableCells ) ) ;
         }
         Row tempRow = Row( children : tempRowChildren );
         columnChildren.add( tempRow ) ;
@@ -128,4 +157,5 @@ class _CanvasState extends State<Canvas>{
   }
 
 }
+
 
